fa098b586cc9f1444bd4d5aca12436b5a60d1969
simpsonjulian <simpsonjulian@gmail.com>
2011-11-08 21:37:47 +0000

    and let's clean out the droppings

--- release.sh
+++ release.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 set -e # let's *not* keep on trucking
+rm *.releaseBackup
+rm release.properties
 
 mvn --batch-mode -Dtag=my-proj-1.3 release:prepare \
                  -DreleaseVersion=1.3 \
