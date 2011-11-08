9baf9da8e796d0ed868cb4c8d956359ad47cf660
simpsonjulian <simpsonjulian@gmail.com>
2011-11-08 21:56:20 +0000

    let's try again for the next release

--- release.sh
+++ release.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -e # let's *not* keep on trucking
-rm *.releaseBackup
-rm release.properties
+rm -f *.releaseBackup
+rm -f release.properties
 
 mvn --batch-mode -Dtag=my-proj-1.3 release:prepare \
                  -DreleaseVersion=1.3 \
-                 -DdevelopmentVersion=2.0-SNAPSHOT
+                 -DdevelopmentVersion=1.4-SNAPSHOT
 mvn --batch-mode release:perform
