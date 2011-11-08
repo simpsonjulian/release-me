6501bc53aeabfd759240f18a86ec8b971da875bf
simpsonjulian <simpsonjulian@gmail.com>
2011-11-08 22:14:40 +0000

    nopers

--- release.sh
+++ release.sh
@@ -3,7 +3,7 @@ set -e # let's *not* keep on trucking
 rm -f *.releaseBackup
 rm -f release.properties
 
-mvn --batch-mode -Dtag=my-proj-1.3 release:prepare \
-                 -DreleaseVersion=1.3 \
-                 -DdevelopmentVersion=1.4-SNAPSHOT
+mvn --batch-mode -Dtag=my-proj-1.5 release:prepare \
+                 -DreleaseVersion=1.5 \
+                 -DdevelopmentVersion=1.6-SNAPSHOT
 mvn --batch-mode release:perform
