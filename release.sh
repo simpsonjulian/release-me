35e3a6389f0c0f861152b2b14b95e711a6c15abb
simpsonjulian <simpsonjulian@gmail.com>
2012-01-19 22:36:29 +0000

    lose dross

--- release.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e # let's *not* keep on trucking
-rm -f *.releaseBackup
-rm -f release.properties
-
-mvn --batch-mode -Dtag=my-proj-1.5 release:prepare \
-                 -DreleaseVersion=1.5 \
-                 -DdevelopmentVersion=1.6-SNAPSHOT
-mvn --batch-mode release:perform
